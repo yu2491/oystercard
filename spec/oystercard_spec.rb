4df659097d1fe460638dcacda4293ef5afae289d
joshSteadman <josh.steadman15@gmail.com>
2020-08-10 15:25:11 +0100

    check if Oystercard creates instances

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,8 @@
+require 'oystercard'
 describe Oystercard do
+
+    it 'gets an instance of Oystercard' do
+        oyster = Oystercard.new
+        expect(oyster).to be_instance_of Oystercard
+    end
 end
