1d6082d39934eeb142a067ab824fc633e16eb625
joshSteadman <josh.steadman15@gmail.com>
2020-08-10 15:31:26 +0100

    check for balance to equal 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,10 @@ describe Oystercard do
         oyster = Oystercard.new
         expect(oyster).to be_instance_of Oystercard
     end
+
+  describe '#initialize' do
+    it "oystercard balance is initialized to 0" do
+        expect(subject.balance).to eq 0
+    end
+  end
 end
