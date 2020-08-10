a61633704b7b4acb8226a663e2e659f18970aaca
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-10 15:50:13 +0100

    top_up method test failed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,15 @@
 require 'oystercard'
 describe Oystercard do
 
-    it 'gets an instance of Oystercard' do
-        oyster = Oystercard.new
-        expect(oyster).to be_instance_of Oystercard
-    end
-
   describe '#initialize' do
     it "oystercard balance is initialized to 0" do
         expect(subject.balance).to eq 0
     end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+
   end
 end
