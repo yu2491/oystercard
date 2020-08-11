e780dd1fed0be3e5368da7cc22a5e5406bcadd99
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-11 15:58:27 +0100

    station name and zone created

--- lib/station.rb
+++ lib/station.rb
@@ -1,2 +1,8 @@
-class Station 
+class Station
+  def initialize(name,zone)
+    @name = name
+    @zone = zone
+  end
+
+  attr_reader :name, :zone
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,8 +3,18 @@ require 'station'
 describe Station do
 
   it "station exists" do
-    station = Station.new
+    station = Station.new('name',1)
     expect(station).to be_an_instance_of(Station)
+  end
+
+  it "has a name" do
+    station = Station.new('name',1)
+    expect(station.name).to eq 'name'
+  end
+
+  it "has a zone" do
+    station = Station.new('name',1)
+    expect(station.zone).to eq 1
   end
 
 end
