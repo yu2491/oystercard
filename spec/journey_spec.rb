c0715003ed32b6289084455ae335ca01a0b75657
imogenkraak <imogen.kraak@gmail.com>
2020-08-12 15:31:09 +0100

    Added two tests to journey spec file

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,11 +1,18 @@
 require "journey"
 
-describe Journey do 
+describe Journey do
 
-  it "starts the journey" do 
-    card = double("card", :touch_in => "Old Street")
-    expect(subject.start).to eq "Old Street"
+
+  it 'checks if the journey has been completed' do
+    expect(subject).not_to be_complete
   end
 
+  it 'charges a penalty fare if journey has not been completed' do
+    subject.complete?
+    expect(subject.calculate_fare).to eq(6)
+  end
+  #it 'start_of_journey receives entry_station from Oystercard class' do
+  #  expect(subject.start_of_journey).to eq()
+#  end
 
 end
